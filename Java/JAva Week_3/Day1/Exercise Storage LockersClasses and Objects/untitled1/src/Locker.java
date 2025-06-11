f83da91fd54f06386ea7b5db915f70ac521a66cd
amezab <amezatx@gmail.com>
2025-06-11 09:02:49 -0500

    day 3

--- Java/JAva Week_3/Day1/Exercise Storage LockersClasses and Objects/untitled1/src/Locker.java
+++ Java/JAva Week_3/Day1/Exercise Storage LockersClasses and Objects/untitled1/src/Locker.java
@@ -1,31 +1,62 @@
 public class Locker {
     private String lockerId;
     private boolean isOccupied;
-    public String contents;
+    private String contents;
 
-    public Locker(String lockerId, boolean isOccupied, String contents) {
+    public Locker(String lockerId) {
         this.lockerId = lockerId;
-        this.isOccupied = false;
-        this.contents = "";
+        this.isOccupied = false;// By default, a new locker is not occupied
+        this.contents = ""; //By default, a new locker has no contents
     }
 
+    public String getLockerId() {
+        return lockerId;
+    }
+
+    public void setLockerId(String lockerId) {
+        this.lockerId = lockerId;
+    }
+
+    public boolean isOccupied(){
+        return isOccupied;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 
     public void storeItem(String item) {
-        //Check if the locker is already occupied
-        if(this.isOccupied){
+        if (this.isOccupied) {
             System.out.println("Locker " + this.lockerId + " is already occupied. Cannot store: " + item);
-        }else {
-            //If not occupied, update contents and status
-            this.contents = item;
-            this.isOccupied = true;
+        } else {
+            this.contents = item;      // Set the contents to the new item
+            this.isOccupied = true;    // Mark the locker as occupied
             System.out.println("Locker " + this.lockerId + " now contains: " + this.contents);
         }
     }
 
-    public void removeItem (){
-        this.contents = ""; //clears the content
-
+    public void removeItem() {
+        this.contents = "";      // Clear the contents by setting to an empty string
+        this.isOccupied = false; // Mark the locker as unoccupied
+        System.out.println("Locker " + this.lockerId + " has been emptied.");
     }
+    //I could not figure out how to do this one
+    @Override
+    public String toString() {
+        // Use a ternary operator to make the status more readable
+        String status = this.isOccupied ? "Occupied" : "Empty";
+
+        return "Locker ID: " + this.lockerId +
+                ", Status: " + status +
+                ", Contents: '" + this.contents + "'";
+    }
+
+
+
 
 
 }
